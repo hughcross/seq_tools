98672179a591bca398fb69662a5443b569ac5585
hughcross <hughbcross@gmail.com>
2017-02-15 17:56:51 +0100

    moved functions to module file

--- seq_extractor.py
+++ seq_extractor.py
@@ -3,6 +3,7 @@
 import sys
 import argparse
 from Bio import SeqIO
+import seq_functions.seq_funcs
 
 parser = argparse.ArgumentParser(description='extract sequences from a list of IDs')
 
@@ -20,28 +21,6 @@ outseq = args.output
 listfile = args.listing
 
 
-def make_list(file_list):
-    """convert lines in file to elements of list. for make_list"""
-    file=open(file_list, 'r')
-    new_list = []
-    for line in file:
-        line = line.strip('\n')
-        new_list.append(line)
-    return new_list
-
-def extract_seqs(seq_set, seq_file_name, new_filename='extracted'):
-    """allows iteration over many take input list of sequence ids and extract them from input filename to new fasta file"""
-    if new_filename == 'extracted':
-        new_name = 'extracted_'+seq_file_name
-    else:
-        new_name = new_filename
-
-    new_file = open(new_name, 'w')
-    for seq in SeqIO.parse(seq_file_name, 'fasta'):
-        rec_id = seq.id
-        if rec_id in seq_set:
-            SeqIO.write(seq, new_file, 'fasta')
-    new_file.close()
 
 seqlist = make_list(listfile)
 
@@ -49,10 +28,10 @@ seq_set = set(seqlist)
 
 if args.output:
 
-    extract_seqs(seq_set, infile, outseq)
+    seq_funcs.extract_seqs(seq_set, infile, outseq)
 
 else:
-    extract_seqs(seq_set, infile)
+    seq_funcs.extract_seqs(seq_set, infile)
 
 print("all done")
 
---
+++ seq_functions/seq_funcs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+
+from Bio import SeqIO
+
+def make_list(file_list):
+    """convert lines in file to elements of list. for make_list"""
+    file=open(file_list, 'r')
+    new_list = []
+    for line in file:
+        line = line.strip('\n')
+        new_list.append(line)
+    return new_list
+
+def extract_seqs(seq_set, seq_file_name, new_filename='extracted'):
+    """allows iteration over many take input list of sequence ids and extract them from input filename to new fasta file"""
+    if new_filename == 'extracted':
+        new_name = 'extracted_'+seq_file_name
+    else:
+        new_name = new_filename
+
+    new_file = open(new_name, 'w')
+    for seq in SeqIO.parse(seq_file_name, 'fasta'):
+        rec_id = seq.id
+        if rec_id in seq_set:
+            SeqIO.write(seq, new_file, 'fasta')
+    new_file.close()
+
